055b01cf51569f590977325032f41d1e0ce48916
mikkybang <mefemena@gmail.com>
2017-10-20 15:20:29 +0100

    Initial Commit

--- app.js
+++ app.js
@@ -139,7 +139,7 @@ app.get('/delete/:studentID', function(req,res){
 
 });
 //start server
-var server = app.listen(8000, function () {
+var server = app.listen(80, function () {
 var host = server.address().address
 var port = server.address().port
 console.log("Example app listening at http://%s:%s", host, port)
